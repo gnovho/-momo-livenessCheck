eb7241ec1b96c109d21c924f917637aaf39e1252
Vong_Ho_Anh <vong.ho@mservice.com.vn>
2021-03-16 18:33:47 +0700

    [Pretrain] adding video to pretrain

--- train_liveness.py
+++ train_liveness.py
@@ -1,5 +1,5 @@
 # USAGE
-# python train_liveness.py --dataset dataset --model liveness.model --le le.pickle
+# python train_liveness.py --dataset dataset --model ./model --le le.pickle
 
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
@@ -37,7 +37,7 @@ args = vars(ap.parse_args())
 # epochs to train for
 INIT_LR = 1e-4
 BS = 8
-EPOCHS = 50
+EPOCHS = 30
 
 # grab the list of images in our dataset directory, then initialize
 # the list of data (i.e., images) and class images
@@ -106,15 +106,15 @@ f = open(args["le"], "wb")
 f.write(pickle.dumps(le))
 f.close()
 
-# plot the training loss and accuracy
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, EPOCHS), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, EPOCHS), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, EPOCHS), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, EPOCHS), H.history["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy on Dataset")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
+###plot the training loss and accuracy
+# plt.style.use("ggplot")
+# plt.figure()
+# plt.plot(np.arange(0, EPOCHS), H.history["loss"], label="train_loss")
+# plt.plot(np.arange(0, EPOCHS), H.history["val_loss"], label="val_loss")
+# plt.plot(np.arange(0, EPOCHS), H.history["acc"], label="train_acc")
+# plt.plot(np.arange(0, EPOCHS), H.history["val_acc"], label="val_acc")
+# plt.title("Training Loss and Accuracy on Dataset")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend(loc="lower left")
+# plt.savefig(args["plot"])
